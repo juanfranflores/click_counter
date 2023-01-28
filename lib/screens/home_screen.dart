b831aded341c307249a429a1e31146d8df631ce6
juanfranflores <juan27fp@gmail.com>
2023-01-28 15:03:41 -0300

    creo pantalla de inicio

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[400],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Hola Mundo!',
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
